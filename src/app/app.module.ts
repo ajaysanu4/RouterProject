e6e2ab129b315dec181628b1f00eaa78c5b58783
ajaysanu4 <48160743+ajaysanu4@users.noreply.github.com>
2019-07-06 13:14:26 +0530

    7/6/2019

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PointsTableComponent } from './points-table/points-table.component';
+import { UpcomingMatchesComponent } from './upcoming-matches/upcoming-matches.component';
+import { NewsComponent } from './news/news.component';
+import { RouterModule, Routes} from '@angular/router'
+
+const route:Routes=[
+  
+    {path:'points',component:PointsTableComponent},
+    {path:'news', component:NewsComponent},
+    {path:'Upcommingmatches', component:UpcomingMatchesComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PointsTableComponent,
+    UpcomingMatchesComponent,
+    NewsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(route)
   ],
   providers: [],
   bootstrap: [AppComponent]
